3a4c199848a0ef67683508e08778bc90898197b3
simon gomez <gomezimon.ernesto@gmail.com>
2024-07-14 18:16:44 -0400

    Grid en inicio

--- js/inicio.js
+++ js/inicio.js
@@ -1,12 +1,22 @@
 const apiKey = 'ktkXwu0P-mMXC118SIHBgu8cRq_J6zyLCPS6c6Q1FOk';
 const query = 'soccer';
-const url = `https://api.unsplash.com/photos/random?query=${query}&client_id=${apiKey}`;
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const imageUrl = data.urls.regular;
-        const imageElement = document.getElementById('randomImg');
-        imageElement.src = imageUrl;
-        imageElement.alt = data.alt_description || 'Imagen de fútbol';
-    })
+function fetchRandomImagen(element) {
+    const url = `https://api.unsplash.com/photos/random?query=${query}&client_id=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const imageUrl = data.urls.regular;
+            element.src = imageUrl;
+            element.alt = data.alt_description || 'Imagen de fútbol';
+        })
+        .catch(error => console.error('Error al obtener la imagen:', error));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imageElements = document.querySelectorAll('.randomImg');
+    imageElements.forEach(imgElement => {
+        fetchRandomImagen(imgElement);
+    });
+});
